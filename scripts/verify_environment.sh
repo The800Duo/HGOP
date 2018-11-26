6f0fa5e8b7ce0a9e5ed98751c40daa2a5729adde
Megg <arnimegg@gmail.com>
2018-11-26 17:27:02 +0000

    Added log.txt file with output from script, fixed node version

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 datetime=$(date)
-echo "Welcome, ${USER}"
-echo "This scripts checks the required programs and dependencies"
-echo "Operating system: " $(uname -srm)
-echo "Script started: $datetime"
+> ../log.txt
+echo "Welcome, ${USER}" | tee ../log.txt
+echo "This scripts checks the required programs and dependencies" | tee -a ../log.txt
+echo "Operating system: " $(uname -srm) | tee -a ../log.txt
+echo "Script started: $datetime" | tee -a ../log.txt
+echo $(git --version) | tee -a ../log.txt
+echo "NPM version" $(npm --version) | tee -a ../log.txt
+echo "Nodejs version" $(node -v) | tee -a ../log.txt
 datetime=$(date)
-echo $(git --version)
-echo "NPM version" $(npm --version)
-echo "Nodejs version" $(nodejs --version)
-echo "Script ended: $datetime" 
+echo "Script ended: $datetime" | tee -a ../log.txt
+cat ../log.txt
