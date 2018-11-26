d109ca64781f9935bbca3fb0726fd29ab6b44eec
Baldvin <baldvinj15@ru.is>
2018-11-26 16:57:15 +0000

    added json package and version output of tools

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -5,4 +5,7 @@ echo "This scripts checks the required programs and dependencies"
 echo "Operating system: " $(uname -srm)
 echo "Script started: $datetime"
 datetime=$(date)
+echo $(git --version)
+echo "NPM version" $(npm --version)
+echo "Nodejs version" $(nodejs --version)
 echo "Script ended: $datetime" 
