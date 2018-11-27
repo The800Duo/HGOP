bd7605fd8396b2ea19a28d20b927c4bd6f97b4ca
Megg <arnimegg@gmail.com>
2018-11-27 11:46:44 +0000

    Refactored the code a bit and installed a function for the version output

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+
 datetime=$(date)
-> ../log.txt
-echo "Welcome, ${USER}" | tee ../log.txt
+versionOutput() {
+    echo "$1: " $($1 --version) | tee -a ../log.txt
+}
+
+echo "----------Initialzing txt file for output----------" > ../log.txt
+echo "Welcome, ${USER}" | tee -a ../log.txt
 echo "This scripts checks the required programs and dependencies" | tee -a ../log.txt
 echo "Operating system: " $(uname -srm) | tee -a ../log.txt
+versionOutput git
+versionOutput npm
+versionOutput node
 echo "Script started: $datetime" | tee -a ../log.txt
-echo $(git --version) | tee -a ../log.txt
-echo "NPM version" $(npm --version) | tee -a ../log.txt
-echo "Nodejs version" $(node -v) | tee -a ../log.txt
 datetime=$(date)
 echo "Script ended: $datetime" | tee -a ../log.txt
 cat ../log.txt
