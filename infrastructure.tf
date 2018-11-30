eff910f3f88ee771472484ac4e1163bdaf6926f1
Megg <arnimegg@gmail.com>
2018-11-30 15:40:06 +0000

    Commented the infrastructure file

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,10 +1,12 @@
-# TODO Comment 2-3 sentences.
+# Sets up the credentials for our aws provider with correct region. 
+# Now Terraform can connect to our aws instance.
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   region                  = "us-east-1"
 }
 
-# TODO Comment 2-3 sentences.
+# Declares the aws security groups as GameSecurityGroup(our group). 
+# Opens port for traffic in and out.
 resource "aws_security_group" "game_security_group" {
   name   = "GameSecurityGroup"
 
@@ -30,7 +32,8 @@ resource "aws_security_group" "game_security_group" {
   }
 }
 
-# TODO Comment 2-3 sentences.
+# Sets up the type and name of our aws instance. 
+#Also configures the keypair and security groups.
 resource "aws_instance" "game_server" {
   ami                    = "ami-0ac019f4fcb7cb7e6"
   instance_type          = "t2.micro"
@@ -39,7 +42,8 @@ resource "aws_instance" "game_server" {
   tags {
     Name = "GameServer"
   }
-  # TODO Comment 1-2 sentences.
+  # Copies the initialize game script from the local machine to the aws instance authorized by our keypair.
+  # Now the instance can run the script.
   provisioner "file" {
     source      = "scripts/initialize_game_api_instance.sh"
     destination = "/home/ubuntu/initialize_game_api_instance.sh"
@@ -50,7 +54,8 @@ resource "aws_instance" "game_server" {
       private_key = "${file("~/.aws/GameKeyPair.pem")}"
     }
   }
-  # TODO Comment 1-2 sentences.
+  # Copies the docker-compose file from the local machine to the aws instance authorized by our keypair.
+  # Now the instance can run the .yml file.
   provisioner "file" {
     source      = "docker-compose.yml"
     destination = "/home/ubuntu/docker-compose.yml"
@@ -66,7 +71,8 @@ resource "aws_instance" "game_server" {
   # Since it can take time for the SSH agent on machine to start up we let Terraform
   # handle the retry logic, it will try to connect to the agent until it is available
   # that way we know the instance is available through SSH after Terraform finishes.
-  # TODO Comment 1-2 sentences.
+
+  # Initialzing the script so that we are authorized to run it.
   provisioner "remote-exec" {
     inline = [
       "chmod +x /home/ubuntu/initialize_game_api_instance.sh",
@@ -80,7 +86,7 @@ resource "aws_instance" "game_server" {
   }
 }
 
-# TODO Comment 1-2 sentences.
+# Initializing the variable public_ip as the aws instance public ip.
 output "public_ip" {
   value = "${aws_instance.game_server.public_ip}"
 }
