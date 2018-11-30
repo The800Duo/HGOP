1cae6debc75664bc82cd2cba3185b3b76930d83d
Megg <arnimegg@gmail.com>
2018-11-30 15:55:02 +0000

    finished deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Kill the current session if it is running
+echo "Terminating current session if exists ..."
 terraform destroy -auto-approve
 
 # Start a new session
-printf "Initializing a new terraform session\n"
+echo "Initializing a new terraform session ..."
 terraform init
 
-printf "Applying...\n"
+echo "Applying ..."
 terraform apply -auto-approve
 
 chmod 400 ~/.aws/GameKeyPair.pem
-
 ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
+
+echo "You are now up and running ..."
