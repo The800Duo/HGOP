9521425c6e7838965fae839d750750161ed80b3b
Megg <arnimegg@gmail.com>
2018-12-07 14:41:05 +0000

    Added unit tests for dealer

--- game-api/dealer.unit-test.js
+++ game-api/dealer.unit-test.js
@@ -1,19 +1,101 @@
-/* const dealerConstructor = require('./dealer.js');
+let newDealer = require('./dealer.js');
 
-test('dealer should should shuffle cards', () => {
-  // Arrange
-  const deck = ['a', 'b', 'c'];
-  const dealer = dealerConstructor;
-
-  // Act
-  // Expected Shuffle
-  // ['a', 'b', 'c'] Initial
-  // ['c', 'b', 'a'] Switch index 0 and 2
-  // ['c', 'a', 'b'] Switch index 1 and 2
-   dealer.shuffle(deck), () => {
-        return 0.99;
+function newRandom(randomReturnValues) {
+    let i = 0;
+    return {
+        randomInt: (min, max) => {
+            return randomReturnValues[i++];
+        }
     };
+}
+
+describe('Check if shuffle function works properly', () => {
+    let dealer1;
+    let dealer2;
+    let dependenciesForDealer1;
+    let dependenciesForDealer2;
+    beforeEach(() => {
+        dependenciesForDealer1 = {
+            'random': newRandom([2, 1]),
+        };
+        dependenciesForDealer2 = {
+            'random': newRandom([1, 2, 3]),
+        }
+        dealer1 = newDealer((name) => {
+            return dependenciesForDealer1[name];
+        });
+        dealer2 = newDealer((name) => {
+            return dependenciesForDealer2[name];
+        })
+    });
+    test('dealer should should shuffle cards', () => {
+    // Arrange
+    let deck = ['a', 'b', 'c'];
+
+    // Act
+    dealer1.shuffle(deck);
 
     // Assert
-    expect(deck).toEqual(['c', 'a', 'b']);
-}); */
+    expect(deck).toEqual(['c', 'b', 'a']);
+    });
+
+    test('dealer should should shuffle cards after shuffle', () => {   
+        // Arrange 
+        let deckForDealer1 = ['a', 'b', 'c'];    
+        let deckForDealer2 = ['a', 'b', 'c'];  
+        dealer1.shuffle(deckForDealer1);
+        dealer2.shuffle(deckForDealer2);
+
+        // Assert
+        expect(deckForDealer1).not.toEqual(deckForDealer2);
+    });
+
+    test('that a deck is of correct length', () => {
+        // Arrange
+        let deck = ['a', 'b', 'c', 'd'];
+
+        // Act
+        dealer2.shuffle(deck);
+
+        // Assert
+        expect(deck.length).toEqual(4);
+    });
+});
+
+describe('Check if draw function works properly', () => {
+    let dealer;
+    let dependencies;
+    beforeEach(() => {
+        dependencies = {
+            'random': newRandom([2, 1]),
+        };
+        dealer = newDealer((name) => {
+            return dependencies[name];
+        });
+    });
+    test('that a deck is of correct length after draw', () => {
+        // Arrange
+        let deck = ['a', 'b', 'c'];
+
+        // Act
+        dealer.draw(deck);
+        dealer.draw(deck);
+        
+        // Assert
+        expect(deck.length).toEqual(1);
+    });
+    test('that the correct card is drawn', () => {
+        // Arrange
+        let deck = ['a', 'b', 'c'];
+
+        // Act & Assert
+        expect(dealer.draw(deck)).toEqual('c');
+    });
+    test('that a card from empty deck is undifined', () =>{
+        // Arrange
+        let deck = [ ];
+
+        // Act & Assert
+        expect(dealer.draw(deck)).toEqual(undefined);
+    });
+});
