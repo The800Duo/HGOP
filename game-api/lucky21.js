a567bd39e1d52fc1ea63b44308dc6c36c658388a
Megg <arnimegg@gmail.com>
2018-12-07 22:33:07 +0000

    finished initialzing the docker image and fixed random js for local run

--- game-api/lucky21.js
+++ game-api/lucky21.js
@@ -99,6 +99,13 @@ module.exports = (context) => {
     guessOver21: (game) => {
       game.state.card = dealer.draw(game.state.deck);
     },
+    getState: (game) => {
+			return {
+				cards: game.getCards(game),
+				card: game.getCard(game),
+				finished: game.isGameOver(game),
+			};
+		}
   };
 };
 
