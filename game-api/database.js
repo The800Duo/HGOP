62820f238212d7b623aa2618c6e30cdb5ea13beb
Megg <arnimegg@gmail.com>
2018-12-11 17:16:26 +0000

    Fixed the whole database.js, scope problem for client(notfound) also changed host in .json

--- game-api/database.js
+++ game-api/database.js
@@ -4,117 +4,125 @@ module.exports = function(context) {
   const config = configConstructor(context);
 
   function getClient() {
-    return new Client({
-      host: config.pgHost,
-      user: config.pgUser,
-      password: config.pgPassword,
-      database: config.pgDatabase,
-    });
+      return new Client({
+          host: config.pgHost,
+          user: config.pgUser,
+          password: config.pgPassword,
+          database: config.pgDatabase,
+      });
   }
 
-  setTimeout(() => {
-    const client = getClient();
-    client.connect((err) => {
-      if (err) {
-        console.log('failed to connect to postgres!');
-      } else {
-        console.log('successfully connected to postgres!');
-      }
-    });
-  }, 3000);
-
+  let client = getClient();
+  setTimeout(() =>
+      client.connect((err) => {
+          if (err) {
+            console.log('failed to connect to postgres!');
+          }
+          else{
+            console.log('successfully connected to postgres!');
+          }
+  }), 2000);
+  
   return {
-    insertResult: (won, score, total, onSuccess, onError) => {
-      const client = getClient();
-      client.connect((err) => {
-        if (err) {
-          onError(err);
-          client.end();
-        } else {
-          const query = {
-            text: 'INSERT INTO "History"("Won", "Score", "Total", "InsertedDate") VALUES($1, $2, $3, CURRENT_TIMESTAMP);',
-            values: [won, score, total],
-          };
-          client.query(query, (err) => {
-            if (err) {
-              onError();
-            } else {
-              onSuccess();
-            }
-            client.end();
+
+      insertResult: (won, score, total, onSuccess, onError) => {
+          let client = getClient();
+          client.connect((err) => {
+              if (err) {
+                  onError(err);
+                  client.end();
+              } else {
+                  const query = {
+                      text: 'INSERT INTO "GameResult"("Won", "Score", "Total", "InsertDate") VALUES($1, $2, $3, CURRENT_TIMESTAMP);',
+                      values: [won, score, total],
+                  };
+                  client.query(query, (err) => {
+                      if (err) {
+                        onError();
+                      }
+                      else {
+                        onSuccess();
+                      }
+                      client.end();
+                  });
+              }
           });
-        }
-      });
-      return;
+          return;
+      },
+
+      getTotalNumberOfGames: (onSuccess, onError) => {
+        let client = getClient();
+        client.connect((err) => {
+            if (err) {
+                onError(err);
+                client.end();
+  
+            } else {
+                const query = {
+                    text: `SELECT COUNT(*) FROM "GameResult";`
+                };
+                client.query(query, (err, res) => {
+                    if (err) {
+                      onError();
+                    }
+                    else {
+                      onSuccess(res.rows[0].count);
+                    }
+                    client.end();
+                });
+            }
+        });
+        return;
     },
-    // Should call onSuccess with integer.
-    getTotalNumberOfGames: (onSuccess, onError) => {
-      const client = getCLient();
-      client.connect((err) => {
-        if (err) {
-          onError(err);
-          client.end();
-        } else {
-          const query = {
-            text: `SELECT COUNT(*) FROM "History";`,
-          };
-          client.query(query, (err, res) => {
+
+      getTotalNumberOfWins: (onSuccess, onError) => {
+        let client = getClient();
+        client.connect((err) => {
             if (err) {
-              onError();
+                onError(err);
+                client.end();
+  
             } else {
-              onSuccess(res.rows[0].count);
+                const query = {
+                    text: `SELECT COUNT(*) FROM "GameResult" WHERE "Won"=true;`
+                };
+                client.query(query, (err, res) => {
+                  if (err) {
+                    onError();
+                  }
+                  else {
+                    onSuccess(res.rows[0].count);
+                  }
+                  client.end();
+              });
             }
-            client.end();
-          });
-        }
-      });
-      return;
+        });
+        return;
     },
-    // Should call onSuccess with integer.
-    getTotalNumberOfWins: (onSuccess, onError) => {
-      const client = getCLient();
-      client.connect((err) => {
-        if (err) {
-          onError(err);
-          client.end();
-        } else {
-          const query = {
-            text: `SELECT COUNT(*) FROM "History" WHERE "Won" = true;`,
-          };
-          client.query(query, (err, res) => {
+
+      getTotalNumberOf21: (onSuccess, onError) => {
+        let client = getClient();
+        client.connect((err) => {
             if (err) {
-              onError();
+                onError(err);
+                client.end();
+  
             } else {
-              onSuccess(res.rows[0].count);
+                const query = {
+                    text: `SELECT COUNT(*) FROM "GameResult" WHERE "Score"=21;`
+                };
+                client.query(query, (err, res) => {
+                  if (err) {
+                    onError();
+                  }
+                  else {
+                    onSuccess(res.rows[0].count);
+                  }
+                  client.end();
+              });
             }
-            client.end();
-          });
-        }
-      });
-      return;
-    },
-    // Should call onSuccess with integer.
-    getTotalNumberOf21: (onSuccess, onError) => {
-      const client = getCLient();
-      client.connect((err) => {
-        if (err) {
-          onError(err);
-          client.end();
-        } else {
-          const query = {
-            text: `SELECT COUNT(*) FROM "History" WHERE "Score" = 21;`,
-          };
-          client.query(query, (err, res) => {
-            if (err) {
-              onError();
-            } else {
-              onSuccess(res.rows[0].count);
-            }
-            client.end();
-          });
-        }
-      });
-      return;
+        });
+        return;
     },
   };
 };
