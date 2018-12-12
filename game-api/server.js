dde7e3b3f29692cf6d6bc2e6a118de5660cadd6d
Baldvin <baldvinj15@ru.is>
2018-12-12 15:11:14 +0000

    updated server.js

--- game-api/server.js
+++ game-api/server.js
@@ -10,6 +10,7 @@ module.exports = function(context) {
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 
